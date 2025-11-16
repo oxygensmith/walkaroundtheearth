09fb4ea40aa543a8be36e3d333b2c0442984ce66
Rob Butz <rob@oxygensmith.ca>
2025-11-16 17:21:27 -0600

    Update distance messages.

--- src/js/messages.js
+++ src/js/messages.js
@@ -3,26 +3,34 @@
 export const journeyMessages = [
   {
     km: 0,
-    text: "The beginning and end point of a circumnavigation around the earth is called the 'antipode.'"
+    text: "The beginning and end point of a circumnavigation around the earth is called the 'antipode.'",
   },
   {
-    km: 200,
-    text: "This journey around the equator is 40,041.44 km because the Earth bulges in the middle. If you were to travel over the North and South Poles, you would only move 40,007.863 km."
+    km: 300,
+    text: "This journey around the equator is 40,041.44 km because the Earth bulges in the middle. If you were to travel over the North and South Poles, you would only move 40,007.863 km.",
   },
   {
     km: 500,
-    text: "The distance between two points on a sphere like Earth is calculated with the Haversine formula. It's been in use by sailors since the early 1800s."
+    text: "Later, you'll be able to set waypoints and your angle of travel.<br /><br />But for now, this journey is equatorial.",
   },
   {
-    km: 600,
-    text: "We'll have some fun with destinations later."
+    km: 800,
+    text: "The distance between two points on Earth is calculated with the Haversine formula, to account for the fact that it's a sphere. It's been in use by sailors since the early 1800s.",
+  },
+  {
+    km: 1000,
+    text: "We'll have some fun with destinations later.",
+  },
+  {
+    km: 10010.32,
+    text: "Can you believe that you've made it a quarter of the way?",
   },
   {
     km: 20020.64,
-    text: "Halfway. Congratulations."
+    text: "Halfway. Congratulations.",
   },
   {
     km: 38041.44,
-    text: "You've almost made it. Keep going."
-  }
+    text: "You've almost made it. Keep going.",
+  },
 ];
