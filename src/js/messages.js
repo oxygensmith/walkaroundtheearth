831cd601327a60f0989828044dcdf0bd8d78f6fe
Rob Butz <rob@oxygensmith.ca>
2025-11-16 18:05:03 -0600

    Make our distance messages more interesting.

--- src/js/messages.js
+++ src/js/messages.js
@@ -3,23 +3,35 @@
 export const journeyMessages = [
   {
     km: 0,
-    text: "The beginning and end point of a circumnavigation around the earth is called the 'antipode.'",
+    text: "A 'great circle' is a type of circumnavigation that has the same radius as the Earth itself, the maximum distance you can travel in a straight line - 40,041 km.",
   },
   {
-    km: 300,
-    text: "This journey around the equator is 40,041.44 km because the Earth bulges in the middle. If you were to travel over the North and South Poles, you would only move 40,007.863 km.",
+    km: 150,
+    text: "All lines of longtitude and the equator are 'great circle routes.'",
   },
   {
     km: 500,
+    text: "Actually, so is any straightline path around the earth of the same distance - as long the circle that it makes would slice through the center of the earth.",
+  },
+  {
+    km: 625,
     text: "Later, you'll be able to set waypoints and your angle of travel. | But for now, this journey is equatorial.",
   },
   {
-    km: 800,
-    text: "The distance between two points on Earth is calculated with the Haversine formula, to account for the fact that it's a sphere. It's been in use by sailors since the early 1800s.",
+    km: 725,
+    text: "At least it's the most famous great circle.",
+  },
+  {
+    km: 850,
+    text: "This journey around the equator is the longest great circle, because the Earth bulges in the middle. If you were to circle the globe over the North and South Poles, it would be 67.154 km less distance.",
   },
   {
     km: 1000,
-    text: "We'll have some fun with destinations later.",
+    text: "We'll have some fun with destinations later.|In the meantime, congratulations on your first 1000km.",
+  },
+  {
+    km: 1500,
+    text: "The distance between two points on Earth is calculated with the Haversine formula, to account for the fact that it's a sphere.|It's been in use by sailors since the early 1800s.",
   },
   {
     km: 10010.32,
@@ -27,7 +39,7 @@ export const journeyMessages = [
   },
   {
     km: 20020.64,
-    text: "Halfway. Congratulations.",
+    text: "You've reached the halfway point in your circumnavigation, called the ANTIPODE. Congratulations.",
   },
   {
     km: 38041.44,
